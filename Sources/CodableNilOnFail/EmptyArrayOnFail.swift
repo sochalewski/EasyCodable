44a153ceff707e9ae59555a6a5b7ef73716a59bb
Piotr Sochalewski <sochalewski@gmail.com>
2023-11-02 13:33:49 +0100

    Add @EmptyArrayOnFail

---
+++ Sources/CodableNilOnFail/EmptyArrayOnFail.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+@propertyWrapper
+public struct EmptyArrayOnFail<Value> where Value: Sequence, Value.Element: RawRepresentable {
+    public private(set) var wrappedValue: Value
+    
+    public init(wrappedValue: Value) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+extension EmptyArrayOnFail: Decodable where Value: Decodable, Value.Element: Decodable {
+    public init(from decoder: Decoder) throws {
+        var value = [Value.Element]()
+        guard var container = try? decoder.unkeyedContainer() else {
+            self.wrappedValue = value as! Value
+            return
+        }
+        
+        while !container.isAtEnd {
+            guard let element = try? container.decode(Value.Element.self) else {
+                _ = try? container.decode(EmptyCodable.self)
+                continue
+            }
+            value.append(element)
+        }
+        
+        self.wrappedValue = value as! Value
+    }
+}
+
+extension EmptyArrayOnFail: Encodable where Value: Encodable, Value.Element: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
+    }
+}
+
+extension EmptyArrayOnFail: Equatable where Value: Equatable, Value.Element: Equatable {
+    public static func == (lhs: EmptyArrayOnFail, rhs: EmptyArrayOnFail) -> Bool {
+        lhs.wrappedValue == rhs.wrappedValue
+    }
+}
+
+struct EmptyCodable: Codable {}
--- Tests/CodableNilOnFailTests/CodableNilOnFailTests.swift
+++ Tests/CodableNilOnFailTests/CodableNilOnFailTests.swift
@@ -8,39 +8,75 @@ struct Model: Codable, Equatable {
     
     @NilOnFail var value1: Value?
     @LowercasedNilOnFail var value2: Value?
+    @EmptyArrayOnFail var array: [Value]
 }
 
 final class CodableNilOnFailTests: XCTestCase {
     
     private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
     
     func testSupportedValue() {
-        let json = "{\"value1\":\"first\",\"value2\":\"SEcOnD\"}"
-        let model = try? decoder.decode(
-            Model.self,
-            from: json.data(using: .utf8)!
+        let jsons = [
+            #"{"value1":"first","value2":"SEcOnD","array":["first"]}"#,
+            #"{"value1":"first","value2":"SEcOnD","array":["zeroth", "first", "fifth"]}"#
+        ]
+        let expectedModel = Model(
+            value1: .first,
+            value2: .second,
+            array: [.first]
         )
         
-        XCTAssertEqual(model, .init(value1: .first, value2: .second))
+        jsons.forEach { json in
+            let model = try? decoder.decode(
+                Model.self,
+                from: json.data(using: .utf8)!
+            )
+            
+            XCTAssertEqual(model, expectedModel)
+            
+            XCTAssertEqual(model, try! decoder.decode(Model.self, from: try! encoder.encode(model)))
+        }
     }
     
     func testNilValue() {
-        let json = "{\"value1\":null,\"value2\":null}"
+        let json = #"{"value1":null,"value2":null,"array":null}"#
+        let expectedModel = Model(
+            value1: nil,
+            value2: nil,
+            array: []
+        )
+        
         let model = try? decoder.decode(
             Model.self,
             from: json.data(using: .utf8)!
         )
         
-        XCTAssertEqual(model, .init(value1: nil, value2: nil))
+        XCTAssertEqual(model, expectedModel)
+        
+        XCTAssertEqual(model, try! decoder.decode(Model.self, from: try! encoder.encode(model)))
     }
     
     func testUnsupportedValue() {
-        let json = "{\"value1\":\"third\",\"value2\":\"fourth\"}"
-        let model = try? decoder.decode(
-            Model.self,
-            from: json.data(using: .utf8)!
+        let jsons = [
+            #"{"value1":"third","value2":"fourth","array":["fifth"]}"#,
+            #"{"value1":["third"],"value2":["fourth"],"array":"fifth"}"#
+        ]
+        let expectedModel = Model(
+            value1: nil,
+            value2: nil,
+            array: []
         )
         
-        XCTAssertEqual(model, .init(value1: nil, value2: nil))
+        jsons.forEach { json in
+            let model = try? decoder.decode(
+                Model.self,
+                from: json.data(using: .utf8)!
+            )
+            
+            XCTAssertEqual(model, expectedModel)
+            
+            XCTAssertEqual(model, try! decoder.decode(Model.self, from: try! encoder.encode(model)))
+        }
     }
 }
